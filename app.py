d04fbdbbf77c6458b6ffd60b7cf84a9ebaf27d52
muhammadazanali1141 <alimuhammadazan125@gmail.com>
2026-07-18 13:36:16 +0500

    Enhance NLTK setup and add project attribution
    Added NLTK downloads for stopwords and punkt tokenizer. Included a caption for project attribution.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import PyPDF2
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 stop_words = set(stopwords.words('english'))
 
